4d7946e570baffbfad49ade613b8e136163318b2
vykhand <yozhyg@gmail.com>
2015-09-26 13:03:41 +0200

    Update pyspark_csv.py

--- pyspark_csv.py
+++ pyspark_csv.py
@@ -40,7 +40,9 @@ def csvToDataFrame(sqlCtx,rdd,columns=None,sep=",",parseDate=True, nSampl=1000):
     rdd_sql = rdd_array
     if columns is None:
         columns = rdd_array.first()
-        rdd_sampl = rdd_array.zipWithIndex().filter(lambda (r,i): (i > 0 and ((nSampl == 0) or (i < nSampl)))).keys()
+        rdd_sampl = rdd_array.zipWithIndex().filter(lambda (r,i): (i > 0)
+        if nSampl > 0: rdd_sampl = rdd_sampl.filter(lambda(r,i): (i < nSampl)
+        rdd_sampl = rdd_sampl.keys()
         rdd_sql = rdd_array.zipWithIndex().filter(lambda (r,i): i > 0).keys()
     column_types = evaluateType(rdd_sampl,parseDate)
     def toSqlRow(row):
